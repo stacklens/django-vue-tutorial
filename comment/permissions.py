b125fbad96e2c6ed3d0b03a30ae24fea991b3181
Dusai <dusaiphoto@foxmail.com>
2020-12-28 14:07:34 +0800

    p150

--- comment/permissions.py
+++ comment/permissions.py
@@ -4,7 +4,32 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsOwnerOrReadOnly(BasePermission):
     message = 'You must be the owner to update.'
 
-    def has_object_permission(self, request, view, obj):
+    def safe_methods_or_owner(self, request, func):
         if request.method in SAFE_METHODS:
             return True
-        return obj.author == request.user
+
+        return func()
+
+    def has_permission(self, request, view):
+        return self.safe_methods_or_owner(
+            request,
+            lambda: request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        return self.safe_methods_or_owner(
+            request,
+            lambda: obj.author == request.user
+        )
+
+    # def has_permission(self, request, view):
+    #     if request.method in SAFE_METHODS:
+    #         return True
+    #
+    #     return request.user.is_authenticated
+    #
+    # def has_object_permission(self, request, view, obj):
+    #     if request.method in SAFE_METHODS:
+    #         return True
+    #
+    #     return obj.author == request.user
