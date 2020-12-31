df0d841b8f5a2155244583a68eedf0654c7ffeb2
Dusai <dusaiphoto@foxmail.com>
2020-12-31 16:31:39 +0800

    p200

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Hello Vue 3!</h1>
+    <p>{{ greeting }} World!</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+
+        data: function () {
+            return {
+                greeting: "Hello12"
+            }
+        }
+    }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
+<!--<template>-->
+<!--<img alt="Vue logo" src="./assets/logo.png">-->
+<!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+<!--</template>-->
+
+<!--<script>-->
+
+<!--import HelloWorld from './components/HelloWorld.vue'-->
+
+
+<!--export default {-->
+<!--name: 'App',-->
+<!--components: {-->
+<!--HelloWorld-->
+<!--}-->
+<!--}-->
+
+<!--</script>-->
+
+<!--<style>-->
+<!--#app {-->
+<!--font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!-- -webkit-font-smoothing: antialiased;-->
+<!-- -moz-osx-font-smoothing: grayscale;-->
+<!--text-align: center;-->
+<!--color: #2c3e50;-->
+<!--margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
